94040430a5c9781c845512f35a3734ae34d251ef
carloscb21 <carloscb495@gmail.com>
2026-03-28 16:41:16 +0100

    chore: normalización de finales de línea y lógica de contexto en Home

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,19 @@
 from django.views.generic import TemplateView
+from blog.models import Post
+from projects.models import Project
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Últimos 3 artículos publicados
+        context['latest_posts'] = Post.objects.filter(status='published').order_by('-published_at')[:3]
+        
+        # Artículo destacado (si existe)
+        context['featured_post'] = Post.objects.filter(featured=True, status='published').order_by('-published_at').first()
+        
+        # 4 Proyectos activos ordenados por el campo order
+        context['active_projects'] = Project.objects.filter(active=True).order_by('order')[:4]
+        
+        return context
